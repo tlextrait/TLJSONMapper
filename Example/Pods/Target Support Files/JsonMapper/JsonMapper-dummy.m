d704d2fe628f844d007d55bd05a4962e17355f4f
Thomas Lextrait <thomas.lextrait@gmail.com>
2017-01-18 14:59:16 +0100

    Updated

---
+++ Example/Pods/Target Support Files/JsonMapper/JsonMapper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JsonMapper : NSObject
+@end
+@implementation PodsDummy_JsonMapper
+@end
---
+++ Example/Pods/Target Support Files/Pods-JsonMapper_Tests/Pods-JsonMapper_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JsonMapper_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JsonMapper_Tests
+@end
